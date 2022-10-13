7539f1b4e1372697f5867d56d1d24cf92da87294
mantik0Ra <kirya_prahov@mail.ru>
2022-10-13 19:17:18 +0400

    feat: add binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,40 +7,93 @@ const { NotImplementedError } = require('../extensions/index.js');
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.rootElement = null;
+    this.bool = false;
+    this.findValue = null;
+  }
+
+
+  Node = class {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.rootElement) {
+      return this.rootElement.value
+    }
+    return this.rootElement
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(value) {
+    const newNode = new this.Node(value);
+    if (!this.rootElement) {
+      this.rootElement = newNode;
+      return
+    }
+
+    let currentNode = this.rootElement;
+
+    while (currentNode) {
+      if (newNode.value < currentNode.value) {
+        if (!currentNode.left) {
+          currentNode.left = newNode;
+          return
+        }
+        currentNode = currentNode.left;
+      } else {
+        if (!currentNode.right) {
+          currentNode.right = newNode;
+          return
+        }
+
+        currentNode = currentNode.right;
+      }
+    }
+
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(hasValue, node = this.rootElement) {
+    if (!node) {
+      return
+    }
+    if (hasValue == node.value) {
+      return this.bool = true;
+    }
+
+    this.has(hasValue, node.left)
+    this.has(hasValue, node.right)
+    return this.bool;
+
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(value, node = this.rootElement) {
+    if (!node) {
+      return
+    }
+    if (value == node.value) {
+      return this.findValue = node.value;
+    }
+
+    this.find(value, node.left)
+    this.find(value, node.right)
+    return this.findValue;
   }
 
   remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
   }
 }
 
