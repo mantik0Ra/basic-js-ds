487faf0bbd6b9bd339833aab50aa1bfdd1c1ad2a
mantik0Ra <kirya_prahov@mail.ru>
2022-10-14 13:49:51 +0400

    feat: add stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.array = [];
+  }
+  push(value) {
+    return this.array.push(value)
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array.pop()
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let top = this.array[this.array.length - 1];
+    return top
   }
 }
 
